ec8b26abd702e9731988868f0499c2a3219c7c50
Liang Shi <liang.shi@protonmail.com>
2017-10-13 10:15:07 +1100

    add more methods and unittest

--- src/Math.php
+++ src/Math.php
@@ -4,7 +4,43 @@ namespace Math;
 
 function m_add($a, $b, $precision = null)
 {
-    return bcadd($a, $b, $precision);
+    return bcadd($a, $b, $precision === null?max_ndecimal($a, $b) : $precision);
+}
+
+function m_div($a, $b, $precision = null)
+{
+  return bcdiv($a, $b, $precision);
+}
+
+function m_sub($a, $b, $precision = null)
+{
+  return bcsub($a, $b, $precision === null ? max_ndecimal($a, $b): $precision);
+}
+
+function m_mul($a, $b, $precision = null)
+{
+  return bcmul($a, $b, $precision);
+}
+
+function m_floor($val, $precision = null)
+{
+    $mul = pow(10, $precision);
+    return bcdiv(floor(bcmul($val, $mul, $precision)), $mul, $precision);
+}
+
+function m_ceil($val, $precision = null)
+{
+    $mul = pow(10, $precision);
+    return bcdiv(ceil(bcmul($val, $mul, $precision)), $mul, $precision);
+}
+
+function m_compare($a, $b)
+{
+  if (version_compare(PHP_VERSION, '7') >= 0) {
+    return ($a <=> $b) === 0;
+  }
+  // todo
+  throw new \Exception('not implemented yet');
 }
 
 function ndecimal($val)
--- tests/MathTest.php
+++ tests/MathTest.php
@@ -3,16 +3,47 @@
 namespace Math\Tests;
 
 use function Math\m_add;
+use function Math\m_sub;
 use function Math\ndecimal;
+use function Math\m_compare;
 use function Math\max_ndecimal;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase;
 
-class MathTest extends PHPUnit_Framework_TestCase
+class MathTest extends TestCase
 {
     public function test_add()
     {
         $this->assertEquals(12, m_add(2, 10));
+        $this->assertEquals(12.1, m_add(2.1, 10));
+        $this->assertEquals(12, m_add(2.1, 10, 0));
+        $this->assertEquals(12.222, m_add(2.122, 10.1));
+        $this->assertEquals(12.10, m_add(2.1, 10.000, 2));
+        $this->assertEquals('0.000000003', m_add('0.000000001', '0.000000002'));
+        $this->assertEquals(0, m_add(0.000000001, 0.000000002, 2));
+        $this->assertEquals(1.000000001, m_add(1, '0.000000001'));
+    }
+
+    public function test_sub()
+    {
+      $this->assertEquals(11, m_sub(21, 10));
+      $this->assertEquals(10.12, m_sub(20.23, 10.11));
+      $this->assertEquals('10.000000000001', m_sub('20.000000000002', '10.000000000001'));
+      $this->assertEquals('10.000000000001', m_sub('20.000000000002', '10.000000000001', null));
+      $this->assertEquals('10', m_sub('20.000000000002', '10.000000000001', 0));
+      $this->assertEquals('10.00', m_sub('20.000000000002', '10.000000000001', 2));
+    }
+
+    public function test_compare()
+    {
+      $this->assertTrue(m_compare(2, 2));
+      $this->assertTrue(m_compare(2, 2.0));
+      $this->assertFalse(m_compare(2, 3));
+      $this->assertTrue(m_compare(2.0E-6, 0.000002));
+      $this->assertTrue(m_compare(2.0E-6, '0.000002'));
+      $this->assertFalse(m_compare(2.0000000001, 2));
+      $this->assertTrue(m_compare(0.000000000001, 0.000000000001));
+      $this->assertTrue(m_compare('0.000000000001', '0.000000000001'));
     }
 
     public function test_decimal_number()
